ca8843565dd19005a00321282c566ed1705b2b4b
Pahlevi Pasha <pahlevi.pasha09@gmail.com>
2022-03-16 14:11:50 +0700

    Add project files.

---
+++ koneksi/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;//sudah terinstall dan tidak error
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace koneksi
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            new Program().Koneksi();
+        }
+
+        public void Koneksi()
+        {
+            using (
+                SqlConnection con = new SqlConnection("data source = (Sesuai dengan server name masing - masing) ;" +
+                "database=ProdiTI;User Id=sa;Password= (Sesuai dengan Password SQL Authentfication masing - masing)")
+                )
+            {
+                con.Open();
+                Console.WriteLine("Koneksi Success");
+            }
+        }
+    }
+}
